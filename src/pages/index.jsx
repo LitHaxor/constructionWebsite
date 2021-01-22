e71ced687fb91af98bed67f72b18efc3f5f5b965
Hasan Ahamed <officials.hasan@gmail.com>
2021-01-23 00:57:36 +0600

    4. Info-designing

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,6 +2,7 @@ import React ,{useState} from 'react'
 import Sidebar from '../components/SideBar';
 import NavBar from '../components/Navbar';
 import Hero from '../components/Hero';
+import InfoSection from '../components/InfoSection';
 const HomePage = () => {
     const [isOpen,setIsOpen] = useState(false);
 
@@ -14,6 +15,7 @@ return (
         <Sidebar isOpen={isOpen} toggle={toggleSidebar} />
         <NavBar toggle={toggleSidebar} />
         <Hero/>
+        <InfoSection/>
     </>
 )
 }
